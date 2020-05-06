141c489c522e43f8da5c3953f43422507847908c
Arisa Rangklang <56140694+arifirst@users.noreply.github.com>
2020-05-06 14:25:39 +0700

    test up git lab6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor(private productService: ProductsService, private cartService: CartService) {
+
+   }
+
+  ngOnInit(): void {
+  }
+
+  getAllProduct(){
+    return this.productService.getAllProduct();
+  }
+
+  addToCart(p_id: number){
+    this.cartService.add(p_id);
+  }
+
+}
